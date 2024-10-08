7afc1f47383d7cd0efbf54e00b522b6029e1e29c
Anton Sorokey <ansorokey@gmail.com>
2024-10-08 13:44:16 -0400

    next card function added to button

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import { hiragana } from './data.js';
 
 let index = 0;
+let currrentDeck = hiragana;
 
 let nextButton = document.querySelector('#nextButton');
 let prevButton = document.querySelector('#prevButton');
 let header = document.querySelector('#header');
+let char = document.querySelector('#char');
+let sound = document.querySelector('#sound');
 
-console.log(hiragana[0]);
-header.innerText = hiragana[index].name;
+console.log(currrentDeck[0]);
+header.innerText = currrentDeck[index].name;
+char.innerText = currrentDeck[index].char;
+sound.innerText = currrentDeck[index].sound;
+
+function nextCard() {
+    index += 1;
+    if (index >= currrentDeck.length) index = 0;
+
+    header.innerText = currrentDeck[index].name;
+    char.innerText = currrentDeck[index].char;
+    sound.innerText = currrentDeck[index].sound;
+}
+
+nextButton.addEventListener('click', nextCard);
